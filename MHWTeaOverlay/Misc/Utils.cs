0574154b599ae90fcef1b70edb25d99cb4e46520
GreenComfyTea <klunics97@gmail.com>
2024-02-13 18:13:55 +0200

    Separate ColorUtils from Utils

--- MHWTeaOverlay/Misc/Utils.cs
+++ MHWTeaOverlay/Misc/Utils.cs
@@ -1,14 +1,16 @@
-﻿using System;
+﻿using Reloaded.Hooks;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace MHWTeaOverlay;
 
-public class Utils
+public static class Utils
 {
 	public static int Clamp(int value, int min, int max)
 	{
@@ -38,20 +40,5 @@ public class Utils
 	public static bool IsApproximatelyEqual(float a, float b)
 	{
 		return Math.Abs(a - b) <= Constants.EPSILON;
-	}
-
-	public int ColorToArgb(Color color, float opacityScale = 1f)
-	{
-		if (!IsApproximatelyEqual(opacityScale, 1f))
-		{
-			return color.ToArgb();
-		}
-
-		byte alpha = (byte) Math.Round(opacityScale * color.A);
-		byte red = color.R;
-		byte green = color.G;
-		byte blue = color.B;
-
-		return Color.FromArgb(alpha, color).ToArgb();
 	}
 }
