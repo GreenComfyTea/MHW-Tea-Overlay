2b87d92346b8ec1c9720576a04cce14603811fc1
GreenComfyTea <klunics97@gmail.com>
2024-02-13 18:41:33 +0200

    Draw: Implement basic Filled Rectangle and Outline Rectangle

--- MHWTeaOverlay/OverlayUI/Draw.cs
+++ MHWTeaOverlay/OverlayUI/Draw.cs
@@ -11,6 +11,9 @@ namespace MHWTeaOverlay
 {
 	public class Draw
 	{
+		// Draw List uses int color in ABGR format
+		// ImGui uses Vector4 color in RGBA format
+
 		// Singleton Pattern
 		private static readonly Draw singleton = new();
 
@@ -28,9 +31,38 @@ namespace MHWTeaOverlay
 			foregroundDrawList = ImGui.GetForegroundDrawList();
 		}
 
-		public void DrawRectangle()
+		public void OutlineRectangle(Vector2 position, SizeF size, uint color, float thickness)
 		{
-			foregroundDrawList.AddRect(new Vector2(100, 100), new Vector2(150, 150), 0x1a334d66);
+			foregroundDrawList = ImGui.GetForegroundDrawList();
+
+			foregroundDrawList.AddRect(position, position + size.ToVector2(), color, 0f, 0x0, thickness);
+		}
+
+		public void OutlineRectangle(float x, float y, float width, float height, uint color, float thickness)
+		{
+			foregroundDrawList = ImGui.GetForegroundDrawList();
+
+			var position = new Vector2(x, y);
+			var size = new Vector2(width, height);
+
+			foregroundDrawList.AddRect(position, position + size, color, 0f, 0x0, thickness);
+		}
+
+		public void FilledRectangle(Vector2 position, SizeF size, uint color)
+		{
+			foregroundDrawList = ImGui.GetForegroundDrawList();
+
+			foregroundDrawList.AddRectFilled(position, position + size.ToVector2(), color, 0f);
+		}
+
+		public void FilledRectangle(float x, float y, float width, float height, uint color)
+		{
+			foregroundDrawList = ImGui.GetForegroundDrawList();
+
+			var position = new Vector2(x, y);
+			var size = new Vector2(width, height);
+
+			foregroundDrawList.AddRectFilled(position, position + size, color, 0f);
 		}
 	}
 }
