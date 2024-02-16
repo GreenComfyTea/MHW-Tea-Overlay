a3565201f1585c8a160345a226f0e06372e5b8de
GreenComfyTea <klunics97@gmail.com>
2024-02-16 11:56:03 +0200

    Update Constants

--- MHWTeaOverlay/Misc/Constants.cs
+++ MHWTeaOverlay/Misc/Constants.cs
@@ -12,10 +12,11 @@ public enum FillDirections { LeftToRight, RightToLeft, TopToBottom, BottomToTop 
 
 public static class Constants
 {
+	public const string MOD_NAME = "Tea Overlay";
+
 	public const float EPSILON = 0.000001f;
 
 	public const string DEFAULT_LOCALIZATION = "en-us";
-	public const string DEFAULT_CONFIG = "default";
 
 	public const string PLUGIN_PATH = @"nativePC\plugins\CSharp\MHWTeaOverlay\";
 	public const string PLUGIN_DATA_PATH = $@"{PLUGIN_PATH}data\";
@@ -23,8 +24,13 @@ public static class Constants
 	public const string CONFIGS_PATH = $@"{PLUGIN_DATA_PATH}configs\";
 	public const string FONTS_PATH =  $@"{PLUGIN_DATA_PATH}fonts\";
 
+	public const string DEFAULT_CONFIG = "default";
+	public const string DEFAULT_CONFIG_WITH_EXTENSION = $"{DEFAULT_CONFIG}.json";
+	public const string DEFAULT_CONFIG_FILE_PATH_NAME = $"{PLUGIN_DATA_PATH}{DEFAULT_CONFIG_WITH_EXTENSION}";
 
-	public const string CONFIG_FILE_PATH_NAME = $"{PLUGIN_DATA_PATH}config.json";
+	public const string SELECTED_CONFIG = "config";
+	public const string SELECTED_CONFIG_WITH_EXTENSION = $"{SELECTED_CONFIG}.json";
+	public const string SELECTED_CONFIG_FILE_PATH_NAME = $"{PLUGIN_DATA_PATH}{SELECTED_CONFIG_WITH_EXTENSION}";
 
 	public const float DRAG_FLOAT_SPEED = 0.1f;
 	public const float DRAG_FLOAT_MAX = 15360f;
